7d6209212a787c408efaf42521bf4b4868ca6ad4
Kabosu <249842959@qq.com>
2017-03-09 20:35:34 +0800

    3.9 20:35

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//var on = true;
+//var off = true;
+//var cc = new Vue({
+//	el: '#app',
+//	data: {
+//		ok: true
+//	},
+//	methods: {
+//		cnb: function () {
+////			on = false
+////			alert(on)
+//			if(off){
+//				this.ok = false
+//				off = false
+//			}else{
+//				this.ok = true
+//				off = true
+//			}
+//		}
+//	}
+//})
+new Vue({
+	el: '#app',
+	data: {
+    	show: false,
+    	activeClass: 'active_a',
+    	errorClass: 'nav_left'
+	}
+})
+// console.log(show)
+
+//var a = 10
+//var b = a+++2
+//alert(b)
