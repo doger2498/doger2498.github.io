7eab794b56b7f58155fb2d2b7591b4e23c6291bf
Kabosu <249842959@qq.com>
2017-03-17 13:46:23 +0800

    233

---
+++ app/scripts/controllers/styleModality.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trainingProjectsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trainingProjectsApp
+ */
+angular.module('trainingProjectsApp')
+  .controller('styleModalityCtrl', function ($scope) {
+    $scope.aaa = function(){
+    	alert('dddddddddd')
+    }
+  });
